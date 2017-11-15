f0ca8f7e96540bc12ba866e2f3ea08c8d8643985
mbabanes <scyzoryk03@gmail.com>
2017-11-15 13:16:20 +0100

    add JakiesInnePole in Klasa.java

--- src/main/java/pckg/Klasa.java
+++ src/main/java/pckg/Klasa.java
@@ -3,6 +3,7 @@ package pckg;
 public class Klasa
 {
     private int pole1;
+    private float jakiesInnePole;
 
     public int getPole1()
     {
@@ -12,5 +13,16 @@ public class Klasa
     public void setPole1(int pole1)
     {
         this.pole1 = pole1;
+    }
+
+
+    public float getJakiesInnePole()
+    {
+        return jakiesInnePole;
+    }
+
+    public void setJakiesInnePole(float jakiesInnePole)
+    {
+        this.jakiesInnePole = jakiesInnePole;
     }
 }
