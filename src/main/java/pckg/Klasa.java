6a176241ef2cc779e3159205bd30f44d3b3b6862
mbabane <asd>
2017-11-15 22:39:53 +0100

    add pole3 in klasa

--- src/main/java/pckg/Klasa.java
+++ src/main/java/pckg/Klasa.java
@@ -4,6 +4,7 @@ public class Klasa
 {
     private int pole1;
     private int pole2;
+    private int pole3;
 
     public int getPole1()
     {
@@ -23,5 +24,15 @@ public class Klasa
     public void setPole2(int pole2)
     {
         this.pole2 = pole2;
+    }
+
+    public int getPole3()
+    {
+        return pole3;
+    }
+
+    public void setPole3(int pole3)
+    {
+        this.pole3 = pole3;
     }
 }
