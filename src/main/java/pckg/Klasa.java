081c3cbe247334176aac98be3814de2ed8eb23e8
mbabane <asd>
2017-11-15 13:12:08 +0100

    add Klasa om pckg package

---
+++ src/main/java/pckg/Klasa.java
@@ -0,0 +1,16 @@
+package pckg;
+
+public class Klasa
+{
+    private int pole1;
+
+    public int getPole1()
+    {
+        return pole1;
+    }
+
+    public void setPole1(int pole1)
+    {
+        this.pole1 = pole1;
+    }
+}
